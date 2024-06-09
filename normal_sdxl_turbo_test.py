98e470fe53edf4eb49be2e9b5a2da07d79da89dd
TheNetherWatcher <117659673+TheNetherWatcher@users.noreply.github.com>
2024-06-09 13:51:14 +0530

    Update normal_sdxl_turbo_test.py

--- normal_sdxl_turbo_test.py
+++ normal_sdxl_turbo_test.py
@@ -1,10 +1,14 @@
 from diffusers import AutoPipelineForText2Image
 import torch
+import time
 
 pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
 pipe.to("cuda")
 
 prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
 
+t = time.time()
 image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+print(time.time() -t)
+print(image.size)
 image
